3ec514027555c3f9141ec1211ac19359d3d3b610
MadeYoga <vngla21@gmail.com>
2020-09-19 13:39:30 +0700

    Add commands framework implementations

---
+++ NewDiscordBot/Modules/PublicModule.cs
@@ -0,0 +1,65 @@
+﻿using Discord;
+using Discord.Commands;
+using NewDiscordBot.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewDiscordBot.Modules
+{
+    // Modules must be public and inherit from an IModuleBase
+    public class PublicModule : ModuleBase<SocketCommandContext>
+    {
+        public PictureService PictureService { get; set; }
+
+        [Command("ping")]
+        [Alias("pong", "hello")]
+        public Task PingAsync()
+            => ReplyAsync("pong!");
+
+        // Get info on a user, or the user who invoked the command if one is not specified
+        [Command("userinfo")]
+        public async Task UserInfoAsync(IUser user = null)
+        {
+            user = user ?? Context.User;
+
+            await ReplyAsync(user.ToString());
+        }
+
+        [Command("cat")]
+        public async Task GetCatAsync()
+        {
+            // Get a stream containing an image of a cat
+            var stream = await PictureService.GetCatPictureAsync();
+            // Streams must be seeked to their beginning before being uploaded!
+            stream.Seek(0, SeekOrigin.Begin);
+            await Context.Channel.SendFileAsync(stream, "cat.png");
+        }
+
+        [Command("dog")]
+        public async Task GetDogAsync()
+        {
+            Dictionary<String, String> responseDictionary = await PictureService.GetDogUrlAsync();
+            await ReplyAsync(responseDictionary["message"]);
+        }
+
+        [Command("r")]
+        public async Task SearchAsync(String query)
+        {
+            // VERYCOOL, DOESN'T WORK.
+            Console.WriteLine(query);
+            await Context.Channel.SendMessageAsync(query);
+
+            var posts = await Task.Run(() => PictureService.GetPicture(query: query));
+
+            var random = new Random();
+            int randomIndex = random.Next(posts.Count);
+
+            var selectedPost = posts[randomIndex];
+            Console.WriteLine(selectedPost);
+            Console.WriteLine(selectedPost.GetType());
+        }
+    }
+}
---
+++ NewDiscordBot/Services/PictureService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using Reddit;
+using Reddit.Controllers;
+using Reddit.Inputs.Search;
+using Newtonsoft.Json;
+
+namespace NewDiscordBot.Services
+{
+    public class PictureService
+    {
+        private readonly HttpClient client;
+        private RedditClient redditClient;
+
+        public PictureService(HttpClient _client)
+        {
+            client = _client;
+
+            redditClient = new RedditClient(
+                appId: Environment.GetEnvironmentVariable("REDDIT_CLIENT_ID"), 
+                appSecret: Environment.GetEnvironmentVariable("Reddit_CLIENT_SECRET"),
+                refreshToken: Environment.GetEnvironmentVariable("REDDIT_USER_AGENT")
+                );
+        }
+
+        public async Task<Stream> GetCatPictureAsync()
+        {
+            var resp = await client.GetAsync("https://cataas.com/cat");
+            return await resp.Content.ReadAsStreamAsync();
+        }
+
+        public async Task<Dictionary<String, String>> GetDogUrlAsync()
+        {
+            var resp = await client.GetAsync("https://dog.ceo/api/breeds/image/random");
+            var jsonString = await resp.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Dictionary<String, String>>(jsonString);
+        }
+
+        public List<Post> GetPicture(String query, String subreddit = "")
+        {
+            List<Post> posts = redditClient.Subreddit(subreddit).Search(new SearchGetSearchInput("Bernie Sanders"));
+            if (posts.Count == 0)
+            {
+                posts = redditClient.Search(new SearchGetSearchInput(query));
+            }
+            return posts;
+        }
+
+    }
+}
