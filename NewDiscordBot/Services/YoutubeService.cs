49c9e0a9f693c2c2943854e10bf0bf61a64ff5a7
MadeYoga <vngla21@gmail.com>
2020-09-21 11:45:54 +0700

    Add YoutubeSearchApi implementation

---
+++ NewDiscordBot/Services/YoutubeService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeSearchAPI;
+
+namespace NewDiscordBot.Services
+{
+    public class YoutubeService
+    {
+        private YoutubeSearchClient ytsClient;
+
+        public YoutubeService()
+        {
+            ytsClient = new YoutubeSearchClient(Environment.GetEnvironmentVariable("DEVELOPER_KEY"));
+        }
+
+        public async Task <dynamic> SearchVideosByQuery(string query)
+        {
+            dynamic response = await ytsClient.Search(query);
+            return response;
+        }
+    }
+}
