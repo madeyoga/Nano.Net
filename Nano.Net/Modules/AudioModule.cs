c02b4358ca91cf25cd8b9ce72dcfe2dca4dc014e
MadeYoga <vngla21@gmail.com>
2020-09-24 17:24:32 +0700

    Add join & leave command, Update Play command

--- Nano.Net/Modules/AudioModule.cs
+++ Nano.Net/Modules/AudioModule.cs
@@ -1,9 +1,14 @@
-﻿using Discord.Commands;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Discord.Audio;
 using Nano.Net.Services;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+using System.Threading.Tasks;
 
 namespace Nano.Net.Modules
 {
@@ -11,11 +16,11 @@ namespace Nano.Net.Modules
     {
         public YoutubeService YoutubeService { get; set; }
 
-        private IAudioClient client;
+        private AudioService audioService;
 
-        public AudioModule(IAudioClient client)
+        public AudioModule(AudioService audioService)
         {
-
+            this.audioService = audioService;
         }
 
         [Command("search")]
@@ -42,7 +47,29 @@ namespace Nano.Net.Modules
         [Command("play")]
         public async Task PlayAsync([Remainder] string query)
         {
-            await SendAsync();
+            await audioService.SendAudioAsync(Context.Guild, query);
+        }
+
+        [Command("join", RunMode = RunMode.Async)]
+        [Alias("summon")]
+        public async Task JoinAsync()
+        {
+            SocketVoiceChannel voiceChannel = (Context.User as SocketGuildUser)?.VoiceChannel;
+
+            if (voiceChannel is null)
+            {
+                await ReplyAsync("Yu r not in a voice channel");
+                return;
+            }
+
+            await audioService.JoinChannel(voiceChannel, Context.Guild.Id);
+        }
+
+        [Command("stop", RunMode = RunMode.Async)]
+        [Alias("leave")]
+        public async Task StopAsync()
+        {
+            await audioService.LeaveChannel(Context);
         }
     }
 }
