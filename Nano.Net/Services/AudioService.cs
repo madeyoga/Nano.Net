a228c0efa89c158b7c999ba4dffd19b186676707
MadeYoga <vngla21@gmail.com>
2020-09-24 17:25:01 +0700

    Update SendAudioAsync method.

--- Nano.Net/Services/AudioService.cs
+++ Nano.Net/Services/AudioService.cs
@@ -1,7 +1,12 @@
-﻿using Discord.Audio;
+﻿using Discord;
+using Discord.Audio;
+using Discord.Commands;
+using Discord.WebSocket;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,26 +14,75 @@ namespace Nano.Net.Services
 {
     public class AudioService
     {
-        public Process CreateStream(string youtubeVideoUrl)
-        {
-            // Stream youtube-dl output to ffmpeg input.
-            string youtubeDlCommand = $"youtube-dl.exe -o - {youtubeVideoUrl} --format bestaudio --quiet --no-warnings";
-            string ffmpegCommand = "ffmpeg -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -i pipe:0 -f asf pipe:1 -vn -nostats -loglevel 0";
+        private readonly ConcurrentDictionary<ulong, IAudioClient> _audioClients = new ConcurrentDictionary<ulong, IAudioClient>();
 
-            var process = Process.Start("cmd.exe",
-                                        $"{youtubeDlCommand} | {ffmpegCommand}");
-            return process;
+        public async Task JoinChannel(IVoiceChannel channel, ulong guildID)
+        {
+            var audioClient = await channel.ConnectAsync();
+            _audioClients.TryAdd(guildID, audioClient);
         }
 
-        public async Task SendAsync(IAudioClient client, string path)
+        public async Task LeaveChannel(SocketCommandContext Context)
         {
-            // Create FFmpeg using the previous example
-            using (var ffmpeg = CreateStream(path))
-            using (var output = ffmpeg.StandardOutput.BaseStream)
-            using (var discord = client.CreatePCMStream(AudioApplication.Mixed))
+            if (_audioClients.TryGetValue(Context.Guild.Id, out IAudioClient audioClient))
             {
-                try { await output.CopyToAsync(discord); }
-                finally { await discord.FlushAsync(); }
+                try
+                {
+                    await audioClient.StopAsync();
+                    _audioClients.TryRemove(Context.Guild.Id, out audioClient);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message + ", cannot disconnect");
+                }
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("I'm not connected");
+            }
+        }
+
+        public Process CreateStream(string youtubeVideoUrl, SocketGuild guild)
+        {
+            // Stream song
+            Process ytdlProcess = Process.Start(new ProcessStartInfo
+            {
+                FileName = "youtube-dl.exe",
+                Arguments = $"--format bestaudio -o Data/Music/{guild.Id}.mp3 {youtubeVideoUrl}",
+                RedirectStandardOutput = true
+            });
+
+            Process ffmpegProcess = Process.Start(new ProcessStartInfo
+            {
+                FileName = "ffmpeg.exe",
+                Arguments = $"-i Data/Music/{guild.Id}.mp3 -ac 2 -f s16le -ar 48000 pipe:1",
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            });
+
+            return ffmpegProcess;
+        }
+
+        public async Task SendAudioAsync(SocketGuild guild, string url)
+        {
+            if (_audioClients.TryGetValue(guild.Id, out IAudioClient client))
+            {
+                using (Stream output = CreateStream(url, guild).StandardOutput.BaseStream)
+                using (AudioOutStream stream = client.CreatePCMStream(AudioApplication.Music))
+                {
+                    try
+                    {
+                        await output.CopyToAsync(stream);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message + " Stopped audio stream");
+                    }
+                    finally
+                    {
+                        await stream.FlushAsync();
+                    }
+                }
             }
         }
 
